c044d1e0f69f0ccb76f48b3022cbde66f45e8559
Joshua Castillo <joshcastillo2048@gmail.com>
2025-01-25 12:11:35 -0500

    Initial commit - xcode vision pro template

---
+++ HealthApp/HealthApp/HealthAppApp.swift
@@ -0,0 +1,33 @@
+//
+//  HealthAppApp.swift
+//  HealthApp
+//
+//  Created by Joshua Castillo on 2025-01-25.
+//
+
+import SwiftUI
+
+@main
+struct HealthAppApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
